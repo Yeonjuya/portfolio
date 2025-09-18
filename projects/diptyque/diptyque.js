5e897030f17857d9bd358164a7735dd882f8db76
연주 <108848057+Yeonjuya@users.noreply.github.com>
2025-09-18 23:53:31 +0900

    Add files via upload

---
+++ projects/diptyque/diptyque.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+
+    // 이메일 복사
+    $('.email-copy').click(function () {
+        var email = $(this).data('email');
+
+        navigator.clipboard.writeText(email).then(function () {
+
+            $('#email-alert').fadeIn(200).delay(2000).fadeOut(200);
+        }).catch(function () {
+
+            var $temp = $('<textarea>');
+            $('body').append($temp);
+            $temp.val(email).select();
+            document.execCommand('copy');
+            $temp.remove();
+            $('#email-alert').fadeIn(200).delay(2000).fadeOut(200);
+        });
+    });
+
+    $('.email-copy').attr('title', '클릭하여 이메일 복사');
+
+    //guide contents animation
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                $(entry.target).addClass("active");
+            }
+        });
+    });
+
+    $(".desktop_img, .mobile_img").each(function () {
+        observer.observe(this);
+    });
+
+
+    // go top
+    // document.querySelector('.go-top').addEventListener('click', function (e) {
+    //     e.preventDefault();
+    //     window.scrollTo({
+    //         top: 0,
+    //         behavior: 'smooth'
+    //     });
+    // });
+
+});
